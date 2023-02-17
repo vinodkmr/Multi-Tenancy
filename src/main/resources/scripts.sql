b803cc17a4cdb6b330131791f93fb1c4c77270a9
vinodkmr <vinodkmr131@gmail.com>
2023-02-17 10:19:46 +0530

    Added Scripts

---
+++ src/main/resources/scripts.sql
@@ -0,0 +1,9 @@
+create table Product (id integer not null, name varchar(255), price float(53) not null, primary key (id));
+
+INSERT INTO PRODUCT VALUES(1, 'CPU', 124);
+INSERT INTO PRODUCT VALUES(2, 'CPU1', 1241);
+
+create table user (id integer not null, name varchar(255), primary key (id));
+
+INSERT INTO user VALUES(1, 'name');
+INSERT INTO user VALUES(2, 'name1');
